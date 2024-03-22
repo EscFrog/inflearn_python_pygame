5da43f983d99cd66431dbe87fd834c5369312c81
Walter Park <neodust223@gmail.com>
2024-03-22 16:53:39 +0900

    볼 쪼개기 작업

--- pang_single_ver/game.py
+++ pang_single_ver/game.py
@@ -81,7 +81,7 @@ balls.append({
   "pos_y": 50,
   "img_idx": 0,
   "to_x": 0.3,
-  "to_y": -0.3,
+  "to_y": -0.8,
   "init_spd_y": ball_speed_y[0]
 })
 
@@ -185,6 +185,40 @@ while isGameOn:
       if weapon_rect.colliderect(ball_rect):
         weapon_to_remove = weapon_idx # 해당 무기 없애기 위한 값 설정
         ball_to_remove = ball_idx # 해당 공 없애기 위한 값 설정
+
+        # 가장 작은 크기의 공이 아니라면 다음 단계의 공으로 나눠주기
+        if ball_img_idx < 3:
+          # 현재 공 크기 정보를 가지고 옴
+          ball_width = ball_rect.width
+          ball_height = ball_rect.height
+
+          # 나눠진 공 정보
+          small_ball_rect = ball_images[ball_img_idx + 1].get_rect()
+          small_ball_width = small_ball_rect.width
+          small_ball_height = small_ball_rect.height
+
+          # 왼쪽으로 튕겨 나가는 공
+          balls.append({
+            "pos_x": ball_pos_x + (ball_width / 2) - (small_ball_width / 2),
+            "pos_y": ball_pos_y + (ball_height / 2) - (small_ball_height / 2),
+            "img_idx": ball_img_idx + 1,
+            "to_x": -0.3,
+            "to_y": -0.8,
+            "init_spd_y": ball_speed_y[ball_img_idx + 1]
+            }
+          )
+
+          # 오른쪽으로 튕겨 나가는 공
+          balls.append({
+            "pos_x": ball_pos_x + (ball_width / 2) - (small_ball_width / 2),
+            "pos_y": ball_pos_y + (ball_height / 2) - (small_ball_height / 2),
+            "img_idx": ball_img_idx + 1,
+            "to_x": 0.3,
+            "to_y": -0.8,
+            "init_spd_y": ball_speed_y[ball_img_idx + 1]
+            }
+          )
+        
         break
   
   # 충돌된 공 or 무기 없애기
